84aebb9ab2f2248f67ef98ac60fe7e771ee66c1b
Luís Eduardo Marinho Fernandes <duduyugi@gmail.com>
2024-02-05 13:12:17 -0300

    setup

---
+++ DictionMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DictionMaster
+//
+//  Created by Luís Eduardo Marinho Fernandes on 05/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
